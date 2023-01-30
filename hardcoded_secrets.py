25c954768e10726f117df7aa56db647730be924e
Manish <62472469+sh377c0d3@users.noreply.github.com>
2023-01-30 23:03:07 +0530

    Update hardcoded_secrets.py

--- hardcoded_secrets.py
+++ hardcoded_secrets.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # Search the code base for sensitive data
     result = search_files(directory, pattern)
     # Save the result to a CSV file
-    save_to_csv(result, 'result.csv')
+    save_to_csv(result, 'hardcoded_secrets_result.csv')
 
 
     
