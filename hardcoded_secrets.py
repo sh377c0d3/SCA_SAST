107c0252ff1d96f12faee2b4378f73a87147ee87
Manish <62472469+sh377c0d3@users.noreply.github.com>
2023-01-30 22:32:27 +0530

    Update hardcoded_secrets.py

--- hardcoded_secrets.py
+++ hardcoded_secrets.py
@@ -6,13 +6,15 @@ def get_pattern():
     # Define the regex pattern to search for sensitive data
     regex_pattern = [
         re.compile(r'(\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b)'|), # email
-        re.compile(r'(\b\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}\b)'), # credit card
+        re.compile(basic [a-zA-Z0-9_\\-:\\.=]+|bearer [a-zA-Z0-9_\\-\\.=]+) # Authorization
+        re.compile(r'(\b\d{4}[- ]?\d{4}[- ]?\d{4}[- ]?\d{4}\b)'|"^4[0-9]{12}(?:[0-9]{3})?$"), # credit and debit card
         re.compile(r'(\b(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}\b)'|[p|P][a|A][s|S][s|S].*\=.*.|[p|P][a|A][s|S][s|S].*\:.*.|[p|P][a|A][s|S].*\:.*.), # password
         re.compile(.........\.[Dd][Bb][^=:]*[:=].*,) # hardcoded database
-        re.compile(crypt_key.*=.*.\w|ssh-rsa*.*,|.secret.*.|.*_rsa|.*_dsa|.*_ed25519|.*_ecdsa|.?ssh/config) # crypto key
+        re.compile(AIza[0-9A-Za-z\\-_]{35}|[0-9]+-[0-9A-Za-z_]{32}\.apps\.googleusercontent\.com|ya29\\.[0-9A-Za-z\\-_]+) # Google key
+        re.compile(crypt_key.*=.*.\w|private.*key|ssh-rsa*.*,|.secret.*.|.*_rsa|.*_dsa|.*_ed25519|.*_ecdsa|.?ssh/config) # crypto key
         re.compile(.pem|.?mysql_history|.?s3cfg|.?htpasswd) # secret files
-        re.compile(.?aws/credentials|private.*key|AKIA[0-9A-Z]|AGPA[0-9A-Z]|AIDA[0-9A-Z]|AIPA[0-9A-Z]|ANPA[0-9A-Z]|ANVA[0-9A-Z]|ASIA[0-9A-Z]|AIza[0-9A-Za-z\\-_]) #aws secrets keys
-        re.compile(jenkins.plugins.publish_over_ssh.BapSshPublisherPlugin.xml|hooks.slack.com) # jenkins and slack path
+        re.compile(.?aws/credentials|(A3T[A-Z0-9]|AKIA|AGPA|AIDA|AROA|AIPA|ANPA|ANVA|ASIA)[A-Z0-9]{16}|amzn\.mws\.[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}) #aws secrets and access keys
+        re.compile(jenkins.plugins.publish_over_ssh.BapSshPublisherPlugin.xml|xox[baprs]-([0-9a-zA-Z]{10,48})?|hooks.slack.com|https://hooks.slack.com/services/T[a-zA-Z0-9_]{10}/B[a-zA-Z0-9_]{10}/[a-zA-Z0-9_]{24}) # jenkins and slack path
     ]
     return regex_pattern
 
